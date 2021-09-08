135e8e7e6bb3c2e51ead7d368d32a09f5157649e
Isac Maximo <88113372+isacmaximo@users.noreply.github.com>
2021-09-08 18:17:30 -0300

    criando uma função das quatro operações básicas
    A função recebe dois números e mostra as quatro operações

---
+++ operations.dart
@@ -0,0 +1,23 @@
+void quatroop(double a, double b) { //quatro operações
+      
+      print('AS QUATRO OPERAÇÕES: ');
+      //soma  
+      double somar = a + b;
+      print('O resultado da soma é: $somar');
+      //subtração
+      double subtrair = a - b;
+      print('O resultado da subtração é: $subtrair');
+      //multiplicação
+      double multiplicar = a * b;
+      print('O resultado da multiplicação é: $somar');
+      //divisão
+      double dividir = a / b;
+      print('O resultado da divisão é: $somar');
+    
+}
+
+
+
+void main() {
+  quatroop(906, 595); //função das quatro operações
+}
